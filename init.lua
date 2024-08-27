d02ff834281aa87c8b082e954e8b5723630f7aac
gerson <data_question@outlook.com>
2024-08-27 10:41:28 -0300

    feat: added the common configuration to the init.lua file

--- init.lua
+++ init.lua
@@ -3,3 +3,8 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 vim.call('plug#end')
+
+home = os.getenv("HOME")
+package.path = home .. "/.config/nvim/?.lua;" .. package.path
+
+require "common"
