42824b57b5896a0e10ebba1cb53ed8ad85047e48
gerson <data_question@outlook.com>
2024-08-27 10:40:48 -0300

    feat: created the common.lua configuration file

---
+++ common.lua
@@ -0,0 +1,22 @@
+-- Show line number
+vim.wo.number = true
+
+-- Enable mouse functionality on the editor
+vim.g.mouse = 'a'
+
+-- Set encoding
+vim.opt.encoding = "utf-8"
+
+-- Disable swapfiles, the thing that enables file recovery
+vim.opt.swapfile = false
+
+-- Set tabulation config
+vim.opt.scrolloff = 7
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.shiftwidth = 4
+vim.opt.fileformat = "unix"
+
+
