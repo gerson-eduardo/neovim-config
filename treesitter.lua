f5c33423907573de019c6f733d4c5ea60e6968f1
gerson <data_question@outlook.com>
2024-08-27 13:28:50 -0300

    feat: created the treesitter config file

---
+++ treesitter.lua
@@ -0,0 +1,6 @@
+require 'nvim-treesitter.configs'.setup{
+	ensure_installed = "all",
+		highlight = {enabled = true},
+		incremental_selection = {enabled = true},
+		textobjects = {enabled = true},
+}
