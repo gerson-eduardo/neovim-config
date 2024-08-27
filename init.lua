a11e63a069832460ee8b236ac913a18e3bbe04f3
gerson <data_question@outlook.com>
2024-08-27 13:29:41 -0300

    feat: added the plugins of my neovim configuration

--- init.lua
+++ init.lua
@@ -2,9 +2,23 @@ local vim = vim
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
+
+Plug("rebelot/kanagawa.nvim")
+Plug("nvim-tree/nvim-tree.lua")
+Plug("nvim-tree/nvim-web-devicons")
+Plug("romgrk/barbar.nvim")
+Plug("nvim-lualine/lualine.nvim")
+Plug("nvim-treesitter/nvim-treesitter", {['do'] = ':TSUpdate'})
+
 vim.call('plug#end')
 
 home = os.getenv("HOME")
 package.path = home .. "/.config/nvim/?.lua;" .. package.path
 
+require "theme"
 require "common"
+require "shortcuts"
+require "vimtree"
+require "barbar"
+require "lua_line"
+require "treesitter"
