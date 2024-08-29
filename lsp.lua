0d9994688a392eb20e530ebfa632d7df2c643d9c
gerson <data_question@outlook.com>
2024-08-29 11:23:04 -0300

    feat: added jdtls to the lsp.conf file

--- lsp.lua
+++ lsp.lua
@@ -1,5 +1,11 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls" }
+	ensure_installed = { "lua_ls", "jdtls" }
 })
-require("lspconfig").lua_ls.setup {}
+
+--require("lspconfig").lua_ls.setup {}
+
+local lspconfig = require('lspconfig')
+
+lspconfig.lua_ls.setup{}
+lspconfig.jdtls.setup{}
